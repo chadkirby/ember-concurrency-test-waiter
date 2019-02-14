fc119c529574554278063b8348c0b9ac30e37a46
Ben Demboski <ben@turbopatent.com>
2019-02-13 17:26:39 -0800

    Update to work with new task property mechanism
    See https://github.com/machty/ember-concurrency/pull/277

--- addon/with-test-waiter.js
+++ addon/with-test-waiter.js
@@ -1,5 +1,5 @@
-import { assert } from '@ember/debug';
 import Ember from 'ember';
+import { computed } from '@ember/object';
 import { registerWaiter } from '@ember/test';
 
 let registered = false;
@@ -9,22 +9,44 @@ let taskRunCounter = 0;
 // waiter so async test helpers will block anytime a task instance spawned
 // from that property is running.
 export default function withTestWaiter(taskProperty) {
-  assert("withTestWaiter() will only work with ember-concurrency >=0.7.19 -- please upgrade", taskProperty.taskFn);
+  let tp = function(...args) {
+    // We want to get the `taskProperty`'s descriptor so that when our getter
+    // is called, we can call its getter to get the task property, and replace
+    // its task function before returning it.
+    // Our first argument is modified in place, so we need to pass a copy of it
+    // to `taskProperty` so its still pristine when we apply the arguments to
+    // our computed property.
+    let argsCopy = [
+      Object.assign({}, args[0]),
+      ...args.slice(1)
+    ];
+    let { descriptor } = taskProperty(...argsCopy);
 
-  let originalTaskFn = taskProperty.taskFn;
+    let cp = computed(function () {
+      let task = descriptor.get.apply(this, arguments);
 
-  taskProperty.taskFn = function *(...args) {
-    if (Ember.testing && !registered) {
-      registerWaiter(() => taskRunCounter === 0);
-      registered = true;
-    }
+      let originalTaskFn = task.fn;
+      task.fn = function *(...args) {
+        if (Ember.testing && !registered) {
+          registerWaiter(() => taskRunCounter === 0);
+          registered = true;
+        }
+    
+        taskRunCounter += 1;
+        try {
+          return yield * originalTaskFn.apply(this, args);
+        } finally {
+          taskRunCounter -= 1;
+        }
+      };
+      task.fn.displayName = originalTaskFn.displayName;
 
-    taskRunCounter += 1;
-    try {
-      return yield * originalTaskFn.apply(this, args);
-    } finally {
-      taskRunCounter -= 1;
-    }
+      return task;
+    });
+
+    return Object.apply.call(cp, cp, args);
   };
-  return taskProperty;
+
+  Ember._setComputedDecorator(tp);
+  return tp;
 }
